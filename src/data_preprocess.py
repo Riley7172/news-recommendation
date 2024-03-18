b1875a3ea6064e7cbceadfadcc1b55e74688cda6
Riley7172 <142705869+Riley7172@users.noreply.github.com>
2024-03-18 20:36:06 +0800

    data_preprocess1.py

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -308,9 +308,9 @@ def transform_entity_embedding(source, target, entity2int_path):
 
 
 if __name__ == '__main__':
-    train_dir = './data/train'
-    val_dir = './data/val'
-    test_dir = './data/test'
+    train_dir = './train'
+    val_dir = './val'
+    test_dir = './test'
 
     print('Process data for training')
 
