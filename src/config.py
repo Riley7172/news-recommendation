47951ed63e3fc8f9a537bef38fa89a4f2458ae6d
Riley7172 <142705869+Riley7172@users.noreply.github.com>
2024-03-19 15:40:56 +0800

    config.py

--- src/config.py
+++ src/config.py
@@ -15,9 +15,9 @@ class BaseConfig():
     num_batches_show_loss = 100  # Number of batchs to show loss
     # Number of batchs to check metrics on validation dataset
     num_batches_validate = 1000
-    batch_size = 128
+    batch_size = 16
     learning_rate = 0.0001
-    num_workers = 4  # Number of workers for data loading
+    num_workers = 0  # Number of workers for data loading
     num_clicked_news_a_user = 50  # Number of sampled click history for each user
     num_words_title = 20
     num_words_abstract = 50
